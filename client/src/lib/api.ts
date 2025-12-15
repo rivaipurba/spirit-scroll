322d2fb6cbf49e297e0e04f994a42bece494d306
Rivai H Purba <rivai22purba@gmail.com>
2025-12-15 10:50:31 +0700

    fix build

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,6 @@
-import { hc } from "hono/client";
-import type { AppType } from "../../../server/src/index";
+// import type { AppType } from "../../../server/src/index";
 
 // MY LOCAL NETWORK IP
 const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/';
 
-export const client = hc<AppType>(BASE_URL);
+export const client = hc<any>(BASE_URL);
